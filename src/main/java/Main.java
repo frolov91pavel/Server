6d9faa67c1185ad2fcde913f360a96e1ae77a312
frolov91pavel <frolov91pavel@mail.ru>
2022-11-05 00:09:14 +0300

    version 1.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
+import java.io.IOException;
 
 public class Main {
-
-    final static int PORT = 8081;
-
     public static void main(String[] args) {
+
         Server server = new Server();
-        server.listen(PORT);
+
+        server.listen(8080);
+
     }
+
+
 
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,26 +1,47 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    int POOL = 64;
-    ExecutorService executorService = Executors.newFixedThreadPool(POOL);
+    final int COUNT = 64;
+
+    private final static Map<String, Map<String, Handler>> handlers = new ConcurrentHashMap<>();
+
+    ExecutorService executorService = Executors.newFixedThreadPool(COUNT);
 
     public void listen(int port) {
 
-        try (final var ServerSocket = new ServerSocket(port)) {
+        try (final var serverSocket = new ServerSocket(port)) {
+
             while (true) {
-                Socket socket = ServerSocket.accept();
-                System.out.println(socket);
-                Handler handler = new Handler(socket);
-                executorService.submit(handler);
+                final var socket = serverSocket.accept();
+                //System.out.println(socket);
+                ClientHandler clientHandler = new ClientHandler(socket);
+                executorService.submit(clientHandler);
             }
+
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
 
     }
 
+    public void addHandler(String method, String path, Handler handler) {
+
+        if (handlers.containsKey(method)) {
+            handlers.get(method).put(path, handler);
+        } else {
+            handlers.put(method, new ConcurrentHashMap<>(Map.of(path, handler)));
+        }
+
+        System.out.println(handlers);
+
+    }
+
+    public static Map<String, Map<String, Handler>> getHandlers() {
+        return handlers;
+    }
 }
